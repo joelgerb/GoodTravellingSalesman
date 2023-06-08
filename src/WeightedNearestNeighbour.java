f92df308b31f395729d7584fcdd996cb45d9b249
16182001 <16182001@L829WD210010.edu.cbe.ab.ca>
2023-06-08 14:29:28 -0600

    g

--- src/WeightedNearestNeighbour.java
+++ src/WeightedNearestNeighbour.java
@@ -8,7 +8,7 @@ public class WeightedNearestNeighbour implements Solver {
 
 	public void solve() {
 		
-		for(int i = 0; i < 10; i++) {
+		for(int i = 0; i < 100; i++) {
 		
 		double totalDistance = 0;
 		Node firstNode;
